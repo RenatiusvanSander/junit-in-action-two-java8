40cbf303ce9adece29526c3b5a975f36f2623d88
RenatiusvanSander / R. Meier <remad@web.de>
2020-08-10 20:08:37 +0200

    Master: second commit

--- src/main/java/edu/remad/chapter03/ErrorResponse.java
+++ src/main/java/edu/remad/chapter03/ErrorResponse.java
@@ -9,7 +9,6 @@ public class ErrorResponse
         implements Response
 {
     private Request originalRequest;
-
     private Exception originalException;
 
     public ErrorResponse( Request request, Exception exception )
